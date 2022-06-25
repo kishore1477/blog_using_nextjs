def9c00572d79e920c0ffd434b2f5f013976051f
kishore1477 <92790475+kishore1477@users.noreply.github.com>
2022-06-25 06:57:49 -0700

    "About page design"

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import styles from '../styles/About.module.css'
 const About = () => {
   return (
-    <div>
-      This is about page
+    <div className={styles.container}>
+      
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel unde voluptate expedita? Rerum, animi maxime! Voluptate illum quae quod nihil esse illo blanditiis et veniam soluta! Fugiat earum iste maxime doloribus sapiente quae sit ipsum magni rerum molestias vel, voluptas accusantium dicta repellat eum. Sequi eveniet distinctio accusamus ratione? Molestias odio, quae recusandae eveniet saepe ullam non maxime sunt animi laudantium voluptatum eligendi voluptatibus eum qui provident nisi, ipsa atque vitae reprehenderit, optio inventore autem perferendis velit. Voluptas ipsa doloremque minima voluptates, sint minus iure amet iste fuga quis optio temporibus aperiam. Distinctio nihil veritatis officia omnis voluptatibus rerum, ipsum reiciendis doloremque quisquam, quasi repellat eveniet! Minima laudantium labore sint, necessitatibus rerum veritatis vel illum odio eius nemo? Non tenetur repellat quod aperiam eius? Adipisci labore porro dolorum recusandae non, alias tenetur nesciunt quaerat cupiditate assumenda odit maxime optio aperiam, consequuntur molestias fuga dignissimos beatae quas, a totam voluptate ducimus accusamus nostrum delectus. Asperiores dignissimos facilis, voluptas deserunt praesentium animi, alias cumque amet incidunt facere delectus ducimus fuga veritatis dolore sequi nemo, molestias dolorum eveniet expedita ullam aliquid ad! Nesciunt sed neque laudantium vel nam qui ullam ut ex, ea voluptate, dolores non ab. Magni expedita ad modi molestias ea.</p>
+<h2>Service</h2>
+<p>service as given below</p>
+<ul>
+  <li>service 1</li>
+  <li>service 2</li>
+  <li>service 3</li>
+  <li>service 4</li>
+  <li>service 5</li>
+  <li>service 6</li>
+  <li>service 7</li>
+  <li>service 8</li>
+  <li>service 9</li>
+  <li>service 10</li>
+</ul>
+<h2>Contact Us</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quibusdam sapiente rem officia corrupti. Corporis voluptatum quis totam sit inventore quam tempora, adipisci, eaque debitis tempore aliquid sint? Corporis pariatur quo consectetur maiores? At expedita dignissimos natus molestiae cum illum fugiat optio beatae veniam deleniti facilis, quos omnis, ducimus soluta?</p>
     </div>
   )
 }
