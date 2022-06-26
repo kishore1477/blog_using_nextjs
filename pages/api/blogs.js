74212c1d8bd8bb600e8ea0f815ae9848af29f4bb
kishore1477 <92790475+kishore1477@users.noreply.github.com>
2022-06-26 06:48:26 -0700

    "InfiniteScroller "

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -8,37 +8,55 @@
 //   res.status(200).json(data)
 // })
 // }
-import * as fs from 'fs';
-export default async function handler(req, res) {
-  let data = await fs.promises.readdir('BlogData')
-  let allblogs;
-  let myfile = []
-  for (let index = 0; index < data.length; index++) {
+// import * as fs from 'fs';
+// export default async function handler(req, res) {
+//   let data = await fs.promises.readdir('BlogData')
+//   let allblogs;
+//   let myfile = []
+//   for (let index = 0; index < data.length; index++) {
 
-    const item = data[index];
-    // console.log(item)
-    allblogs =  await fs.promises.readFile(('BlogData/'+ item), 'utf-8')
-    // console.log(allblogs)
-    myfile.push(JSON.parse(allblogs))   // contvert string to object
+//     const item = data[index];
+//     // console.log(item)
+//     allblogs =  await fs.promises.readFile(('BlogData/'+ item), 'utf-8')
+//     // console.log(allblogs)
+//     myfile.push(JSON.parse(allblogs))   // contvert string to object
 
 
     
-  }
-  // res.status(200).json(allblogs)
-  res.status(200).json(myfile)
+//   }
+//   // res.status(200).json(allblogs)
+//   res.status(200).json(myfile)
 
-//   fs.readdir('BlogData',(req, data)=>{
-//     let allBlogs = []
-//     data.forEach((item)=>{
-//       // console.log(item)
-//       fs.readFile(('BlogData/'+item), (da)=>{
-//         allBlogs.push(da)
-// // console.log(da)
-//       })
-//     })
-    // console.log(data)
+// //   fs.readdir('BlogData',(req, data)=>{
+// //     let allBlogs = []
+// //     data.forEach((item)=>{
+// //       // console.log(item)
+// //       fs.readFile(('BlogData/'+item), (da)=>{
+// //         allBlogs.push(da)
+// // // console.log(da)
+// //       })
+// //     })
+//     // console.log(data)
    
-  // console.log(req)
+//   // console.log(req)
   
-// })
-}
+// // })
+// }
+
+import * as fs from 'fs';
+
+export default async function handler(req, res) {
+
+    console.log(req.query.count)
+    let data = await fs.promises.readdir("BlogData");
+    data = data.slice(0, parseInt(req.query.count)) 
+    let myfile; 
+    let allBlogs = [];
+      for (let index = 0; index < data.length; index++) {
+       
+        const item = data[index]; 
+          myfile = await fs.promises.readFile(('BlogData/' + item), 'utf-8') 
+          allBlogs.push(JSON.parse(myfile))
+      }
+      res.status(200).json(allBlogs)
+    }
--- pages/blog.js
+++ pages/blog.js
@@ -1,57 +1,86 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React, {useState} from 'react'
+import * as fs from 'fs';
+import Link from 'next/link';
 import styles from '../styles/Blog.module.css'
 
-import Link from  'next/link'
-// import { useEffect } from 'react'
-
-
-
-const Blog = (props) => {
+import InfiniteScroll from 'react-infinite-scroll-component';
+const blog = (props) => {
   console.log("This is props")
   console.log(props)
+const [Blogs, setBlogs] = useState(props.allBlogs);
+const [count, setcount] = useState(2);
+// const [count, setcount] = useState(props.allCount);
+console.log("This is Blogs")
+console.log(Blogs)
+console.log(Blogs.length)
 
-  const [Blogs, setBlogs] = useState(props.allblogs);
-  useEffect(() => {
-    console.log("Blog fetch api is working")
-   
-  }, []);
- 
+const fetchData = async()=>{
+ let d = await fetch(`http://localhost:3000/api/blogs/?count=${count + 2}`)
+ setcount(count + 2)
+    let data = await d.json()
+    setBlogs(data)
+}
   return (
     <div className={styles.container}>
-<main className={styles.main}>
- { Blogs.map((item)=>{
-  // console.log(item.title)
-  // console.log(item.content)
-  
-  return <div key={item.slug}>
-  <Link href={`/blogposts/${item.slug}`}>
-  <h3 className={styles.blogitemh3}>{item.title}in  2022?</h3>
-  </Link>
- <p>{item.content.substr(0,40)}...</p>
-  </div>
-  
- })}
+      <main>
+
+      <InfiniteScroll
+        dataLength={Blogs.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={props.allCount !== Blogs.length}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {Blogs.map((blogitem) => {
+          return <div key={blogitem.slug}>
+            <Link href={`/blogposts/${blogitem.slug}`}>
+              <h3 className={styles.blogItemh3}>{blogitem.title}</h3></Link>
+            {/* <p className={styles.blogItemp}>{blogitem.metadesc.substr(0, 3)}...</p> */}
+            <Link href={`/blogposts/${blogitem.slug}`}><button className={styles.btn}>Read More</button></Link>
+          </div>
+        })}
+        {/* This Blogs {Blogs.map((item)=>{
+       return <h1 key={item.slug}>{item.title}</h1>
+           })} */}
+        
+             </InfiniteScroll>
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      </main>
+      </div>
     
-
- 
-     
- 
-    <p>JavaScripts is a language for design  logic for web.</p>
-    </main>
-    </div>
-
   )
-};
-export async function getServerSideProps(context) {
- let data = await  fetch('http://localhost:3000/api/blogs')
- let allblogs = await data.json()
-
-   
-
+}
+export async function getStaticProps(context) {
+  let data = await fs.promises.readdir("BlogData");
+  
+  let allCount = data.length;
+  let myfile;
+  let allBlogs = [];
+    for (let index = 0; index < 2; index++) {
+      const item = data[index];
+        console.log(item)
+        myfile = await fs.promises.readFile(('BlogData/' + item), 'utf-8') 
+        allBlogs.push(JSON.parse(myfile))
+    }
   return {
-    props: {allblogs}, // will be passed to the page component as props
-    // props: {kishore: "Good Boy"}, // will be passed to the page component as props
+    props: {allBlogs,allCount}, // will be passed to the page component as props
   }
 }
-export default Blog
+
+export default blog
