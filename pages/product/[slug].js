0eb4766f080b6433277b6fe8b8d13a74fe86828d
kishore1477 <92790475+kishore1477@users.noreply.github.com>
2022-07-02 07:58:40 -0700

    "creating next app, adding tawilwindcss ,pages"

---
+++ pages/product/[slug].js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+const Slug = () => {
+    const router = useRouter()
+    const { slug } = router.query
+  
+  return (
+    <div>
+      This is slug {slug}
+    </div>
+  )
+}
+
+export default Slug
